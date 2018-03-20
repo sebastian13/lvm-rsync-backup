adc6050d9e015e9d3a157e8ae1cfd47b5ccb6f29
Sebastian Plocek <sebastian@plocek.at>
2018-03-20 18:33:32 +0100

    several improvements

--- README.md
+++ README.md
@@ -1,17 +1,39 @@
 # LVM Rsync Backup
 
-These Bash script help us to backup linux LVs via rsync. This script does not keep multiple versions of the backed up lv, as it is meant to be used on destinations which create snapshots on their own, like SynologyNAS running on btrfs filesystem.
+This Bash script helps us backup linux LVs via rsync. It does not keep multiple versions of the backed up lv, as it is meant to be used on destinations which create snapshots on their own, like SynologyNAS running on btrfs filesystem.
 
 If you want to keep multiple versions, take a look at <https://github.com/bamford/lvm-rsync-backup>.
 
-## Backup a single LV
 1. Mount your backup device to the desired folder, for example to */mnt/backup*
 2. Run script via command or define a cronjob
+3. Run some cronjob at your destination, to keep multiple snapshots
+
+## Parameters
+
+Parameter               | Description                                                       | Necessary
+:---------------------- | :---------------------------------------------------------------- | ---------       
+-a                      | Creates a backup of all mountable Logical Volumes on the system * | if -l not used
+-l [lv_name]            | Creates a backup of provided Logical Volume                       | if -a not used
+-d [/path/to/directory] | Location, where backups will be created                           | optional
+-c                      | Deletes Mountpoints and LVs called *_snapshot                     | optional
+-v [1-7]                | Defines the Log Level                                             | optional
+
+\* This will exclude LVs that have *swp* or *swap* in their name, as they cannot be mounted.
+
+## Examples
+
+### Backup a single LV
 
 ```bash
-lvm-rsync-backup.sh my-lv-disk /mnt/backup
+./lvm-rsync-backup.sh -l lv_home -v7 -d /mnt/backup-nas01
 ```
 
+### Backup all LVs
+```bash
+./backup.sh -a -v7 -d /mnt/backup-nas01
+```
+
+### Crontab
 ```bash
 # The path variable needs to be set
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
@@ -21,29 +43,5 @@ MAILTO="mail@example.com"
 
 # 00:00 Create Snapshot at destination
 # 00:05 Backup my-lv-disk
-5 0 * * * /etc/backup/lvm-rsync-backup.sh my-lv-disk /mnt/backup 2>&1
+5 0 * * * /etc/backup/lvm-rsync-backup.sh -l lv_home -v7 -d /mnt/backup-nas01 2>&1
 ```
-
-Don't forget to create a scheduled task that creates the filesystem-snapshots at your backup destination.
-
-## Backup all LVs
-To backup all LVs currently active on the machine, use the script **backup-lvs.sh**, which uses **lvs** to get the names of every lv. It will exclude LVs that have *swp* or *swap* in their name, as they cannot be mounted. This script uses **lvm-rsync-backup.sh**, therefore save both of them to */etc/backup*.
-
-1. Mount your backup device to the desired folder, for example to */mnt/backup*
-2. Run script via command or define a cronjob
-
-```bash
-/etc/backup/backup-lvs.sh /mnt/backup
-```
-
-```bash
-# The path variable needs to be set
-PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-
-# Sent output to
-MAILTO="mail@example.com"
-
-# 00:00 Create Snapshot at destination
-# 00:05 Backup my-lv-disk
-5 0 * * * /etc/backup/backup-lvs.sh /mnt/backup 2>&1
-``` 
--- lvm-rsync-backup.sh
+++ lvm-rsync-backup.sh
@@ -1,151 +1,214 @@
 #!/bin/bash
 
-# Thanks to
-# https://github.com/bamford/lvm-rsync-backup
+__VERBOSE=7
 
-# This script performs a backup of the specified
-# system logical volume to a backup logical volume
-# located on a separate physical volume.
-# It uses an LVM snapshot to ensure the data
-# remains consistent during the backup.
-
-# Run via Cron like that
-# /etc/backup/lvm-rsync-backup.sh lv_home /mnt/backup &> /var/log/backup-daily-home.log
-# or use backup-all-lvm.sh
-
-# Specify the LV to backup
-# expected format: samba01
-BACKUP_LV=$1
-echo "***"
-echo "*** START backup of $BACKUP_LV"
-echo "***"
-
-# Specify the Backup Mount
-# expected format: /mnt/backup-nas01
-BACKUP_SERVER=$2
-
-# Remove Tailing Slash, if there is one.
-length=${#BACKUP_SERVER}
-last_char=${BACKUP_SERVER:length-1:1}
-[[ $last_char == "/" ]] && BACKUP_SERVER=${BACKUP_SERVER:0:length-1}; 
-echo "BACKUP_SERVER: $BACKUP_SERVER"
-
-BUFFER_SIZE=5G
-
-count=$(lvs | grep -c "\s${BACKUP_LV}\s")
-if [ $count != 1 ]; then
-	echo "There are multiple LVs similar to ${BACKUP_LV}."
-	exit 1
+declare -A LOG_LEVELS
+# https://en.wikipedia.org/wiki/Syslog#Severity_level
+LOG_LEVELS=([0]="emerg" [1]="alert" [2]="crit" [3]="err" [4]="warning" [5]="notice" [6]="info" [7]="debug")
+function .log () {
+local LEVEL=${1}
+shift
+if [ ${__VERBOSE} -ge ${LEVEL} ]; then
+echo "[${LOG_LEVELS[$LEVEL]}]" "$@" >> backup.log 1>&2
 fi
-SOURCE_PATH=$(lvs --noheading -o lv_path | grep $BACKUP_LV | tr -d '  ')
-echo "SOURCE_PATH: $SOURCE_PATH"
+}
 
-BACKUP_DIRECTORY="${BACKUP_SERVER}/${BACKUP_LV}"
-echo "BACKUP_DIRECTORY: $BACKUP_DIRECTORY"
+function clean {
+	.log 7 "clean ()"
+	.log 6 "LV to delete        | $DELETE_LV"
 
-SNAPSHOT_NAME="${BACKUP_LV}_snapshot"
-echo "SNAPSHOT_NAME: $SNAPSHOT_NAME"
+	SNAPSHOT_MOUNT="/mnt/${DELETE_LV}"
+	.log 6 "Snapshot Mountpoint | $SNAPSHOT_MOUNT"
 
-SNAPSHOT_MOUNT="/mnt/${SNAPSHOT_NAME}"
-echo "SNAPSHOT_MOUNT: $SNAPSHOT_MOUNT"
-
-### First check everything was left cleanly last time, and fix if not
-#
-# If the snapshot mountpoint still exists try unmounting and removing it
-#
-if [ -d ${SNAPSHOT_MOUNT} ] ; then
-umount ${SNAPSHOT_MOUNT}
-rmdir ${SNAPSHOT_MOUNT}
-fi
-#
-# If the snapshot logical volume still exists, remove it
-#
-lvdisplay | grep "LV Name" | grep -q ${SNAPSHOT_NAME}
-if [ $? = 0 ]; then
-lvremove -f ${SNAPSHOT_NAME}
-fi
-
-### Create a logical volume to snapshot the system volume
-#
-# This is created every time. The volume is deleted at the end of the
-# backup as it is not necessary to keep it, wastes space and
-# cpu and will freeze when full.
-#
-# The size of this volume needs to be large enough to contain
-# any changes which may happen on the original volume during
-# the course of the backup.  For example, with a size of 592M,
-# if a 1G file is written the snapshot volume may be frozen!
-# To avoid this make size big enough to cope, execute only in
-# quiet times (early hours) and make sure this script completes
-# gracefully if a frozen snapshot is encountered.
-#
-lvcreate -L${BUFFER_SIZE} -s -n ${SNAPSHOT_NAME} ${SOURCE_PATH}
-
-SNAPSHOT_PATH=$(lvs --noheading -o lv_path | grep $SNAPSHOT_NAME | tr -d '  ')
-echo "SNAPSHOT_PATH: $SNAPSHOT_PATH"
-
-### Create the backup logical volume
-#
-# This will only usually be necessary on the first run of this
-# script, unless for some reason the backup itself has been lost!
-#
-# The size of the backup volume must be large enough to contain
-# the contents of the original volume and any changes which have
-# occurred over the length of time for which backups are kept.
-# It may therefore be necessary to extend the backup volume manually.
-#
-
-### Check if Backup Device is mounted. If not, tell user to mount
-### or initizalize Backup Device
-#
-if [ ! -e ${BACKUP_SERVER}/00-backup-mounted ] ; then
-	echo "Backup Device not mounted/initialized. Run touch 00-backup-mounted."
-	exit 1
-fi
-
-### Create the snapshot mount-point
-#
-if [ ! -d ${SNAPSHOT_MOUNT} ] ; then
-mkdir ${SNAPSHOT_MOUNT}
-fi
-
-### Protect the snapshot mount-point
-#
-chmod go-rwx ${SNAPSHOT_MOUNT}
-
-### Mount the snapshot read-only
-#
-mount -o ro ${SNAPSHOT_PATH} ${SNAPSHOT_MOUNT}
-
-if [ $? = 0 ]; then
-
-	### Backup the data
+	# If the snapshot mountpoint still exists try unmounting and removing it
 	#
-	echo "START rsync transfer"
-	rsync -a --delete --delete-excluded --stats \
-		--exclude *_snapshots \
-		--exclude .@upload_cache \
-		--exclude @Recycle \
-		--exclude .papierkorb \
-		--exclude *TemporaryItems \
-		--exclude *DS_Store \
-		${SNAPSHOT_MOUNT}/ ${BACKUP_DIRECTORY}/
+	if [ -d ${SNAPSHOT_MOUNT} ] ; then
+	umount ${SNAPSHOT_MOUNT}
+	rmdir ${SNAPSHOT_MOUNT}
+	fi
+
+	DELETE_PATH=$(lvs --noheading -o lv_path | grep -P "( |$)$DELETE_LV( |$)" | tr -d '  ')
+	.log 6 "Delete Path         | $DELETE_PATH"
+
+	# If the snapshot logical volume still exists, remove it
+	#
+	lvs -o lv_name | grep -e "\s${DELETE_LV}\s" 
+	if [ $? = 0 ]; then
+		.log 6 "Will remove ${DELETE_LV} in 10 seconds!"
+		sleep 10
+		lvremove -f ${DELETE_PATH}
+	fi
+}
+
+function clean-all {
+	.log 7 "clean-all ()"
+	for DELETE_LV in $(lvs --noheading -o lv_name | grep -e 'snapshot' | tr -d '  ')
+	do
+		clean
+	done
+}
+
+function backup {
+	.log 7 "backup ()"
+	.log 6 "LV to backup        | $BACKUP_LV"
+
+	# Remove Tailing Slash, if there is one.
+	length=${#DESTINATION}
+	last_char=${DESTINATION:length-1:1}
+	[[ $last_char == "/" ]] && DESTINATION=${DESTINATION:0:length-1}; 
+	.log 6 "Destination Mount   | $DESTINATION"
+
+	BUFFER_SIZE=10G
+
+	count=$(lvs --noheading -o lv_name | grep -cP "( |$)${BACKUP_LV}( |$)")
+	if [ $count != 1 ]; then
+		.log 3 "There are multiple LVs similar to ${BACKUP_LV}."
+		exit 1
+	fi
+
+	SOURCE_PATH=$(lvs --noheading -o lv_path | grep -P "${BACKUP_LV}( |$)" | tr -d '  ')
+	.log 6 "Source Path         | $SOURCE_PATH"
+
+	BACKUP_DIRECTORY="${DESTINATION}/${BACKUP_LV}"
+	.log 6 "Destination Path    | $BACKUP_DIRECTORY"
+
+	SNAPSHOT_NAME="${BACKUP_LV}_snapshot"
+	.log 6 "Snapshot Name       | $SNAPSHOT_NAME"
+
+	SNAPSHOT_MOUNT="/mnt/${SNAPSHOT_NAME}"
+	.log 6 "Snapshot Mountpoint | $SNAPSHOT_MOUNT"
+
+	lvcreate -L${BUFFER_SIZE} -s -n ${SNAPSHOT_NAME} ${SOURCE_PATH}
+
+	SNAPSHOT_PATH=$(lvs --noheading -o lv_path | grep $SNAPSHOT_NAME | tr -d '  ')
+	.log 6 "Snapshot Path       | $SNAPSHOT_PATH"
+
+	### Check if Backup Device is mounted. If not, tell user to mount
+	### or initizalize Backup Device
+	#
+	if [ ! -e ${DESTINATION}/00-backup-mounted ] ; then
+		.log 3 "Backup Device not mounted/initialized. Run touch 00-backup-mounted."
+		exit 1
+	fi
+
+	### Create the snapshot mount-point
+	#
+	if [ ! -d ${SNAPSHOT_MOUNT} ] ; then
+	mkdir ${SNAPSHOT_MOUNT}
+	fi
+
+	### Protect the snapshot mount-point
+	#
+	chmod go-rwx ${SNAPSHOT_MOUNT}
+
+	### Mount the snapshot read-only
+	#
+	mount -o ro ${SNAPSHOT_PATH} ${SNAPSHOT_MOUNT}
+
+	DF=$(df -hlP ${SNAPSHOT_MOUNT} | awk 'int($5)>80{print "Partition "$1" has only "$4" free."}')
+	
+	if [ "$DF" ]
+	then
+		echo $DF
+	fi
+
+	DF=$(df -h | grep ${SNAPSHOT_MOUNT})
+	.log 5 ${DF}
+
+	if [ ! $NO_RSYNC ]; then
+		### Backup the data
+		#
+		.log 7 "START rsync transfer"
+		rsync -a --delete --delete-excluded --stats -h \
+			--exclude *_snapshots \
+			--exclude .@upload_cache \
+			--exclude @Recycle \
+			--exclude .papierkorb \
+			--exclude *TemporaryItems \
+			--exclude *DS_Store \
+			${SNAPSHOT_MOUNT}/ ${BACKUP_DIRECTORY}/
+	else
+		.log 3 "NO BACKUP WAS CREATED"
+	fi
 
 	### Unmount the Snapshot
 	#
 	umount ${SNAPSHOT_MOUNT}
 
-else
+	### Delete Mountpoint
+	#
+	rmdir ${SNAPSHOT_MOUNT}
 
-	echo "Error Mounting the disk"
-	echo "*** NO BACKUP WAS CREATED ***"
+	### Remove the snapshot volume
+	#
+	lvremove -f ${SNAPSHOT_PATH}
+}
 
+while getopts ":acl:d:nv::" opt; do
+  case $opt in
+    a ) 
+	  BACKUP_ALL=1
+	  ;;
+
+	c )
+	  clean-all
+	  exit 0
+	  ;;
+
+    l )
+      BACKUP_LV=${OPTARG}
+      ;;
+
+    d )
+      DESTINATION=${OPTARG}
+      ;;
+
+    v ) # Verbose Level
+	  __VERBOSE=${OPTARG}
+      ;;
+
+    n )
+      NO_RSYNC=1
+      echo "Disable Rsync"
+      ;;
+
+    h ) # Display help.
+      usage
+      exit 0
+      ;;
+
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  
+  esac
+done
+
+if [ ! "$DESTINATION" ]
+then
+    echo "Please specify a destination using the -d option!"
+    exit 1
 fi
 
-### Delete Mountpoint
-#
-rmdir ${SNAPSHOT_MOUNT}
+if [ "$BACKUP_ALL" ]; then
+	clean-all
 
-### Remove the snapshot volume
-#
-lvremove -f ${SNAPSHOT_PATH}
+	for BACKUP_LV in $(lvs --noheading -o lv_name | grep -v -e 'swap' -e 'swp' | tr -d '  ')
+	do
+		backup
+	done
+else
+	if [ ! "$BACKUP_LV" ]
+	then
+	    echo "Please provide a lv_name using the -l option!"
+	    exit 1
+	fi
+	clean
+	backup
+fi
